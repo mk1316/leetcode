b145c01959fac89c6968925adab03975ee693a83
Kaleb Tsegaye <kfesaha15@gmail.com>
2024-12-24 07:38:41 +0000

    [LeetCode Sync] Runtime - 3 ms (50.37%), Memory - 18.9 MB (33.83%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,15 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
         for i in range(len(nums)):
-            x = nums[i]
-            for j in range(i + 1, len(nums)):
-                y = nums[j]
-                if x + y == target:
-                    return [i, j]
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+            hashmap[nums[i]] = i
 
-
-
-
-
-
-
+            
+            
