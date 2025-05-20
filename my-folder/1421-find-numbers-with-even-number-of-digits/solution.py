15fb998db5b3bc6e67fc288ce18076cf2a996f0d
Kaleb Tsegaye <kfesaha15@gmail.com>
2025-05-20 19:32:56 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.8 MB (54.54%)

---
+++ my-folder/1421-find-numbers-with-even-number-of-digits/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        result = 0
+        for num in nums:
+            if len(str(num)) % 2 == 0:
+                result += 1
+        return result
