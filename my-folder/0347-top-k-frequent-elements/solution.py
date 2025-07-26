971950f5eb08b80d5dc2432ebfe7c5ef45dc1018
Kaleb Tsegaye <kfesaha15@gmail.com>
2025-07-26 22:26:05 +0000

    [LeetCode Sync] Runtime - 16 ms (6.78%), Memory - 24.1 MB (25.12%)

--- my-folder/0347-top-k-frequent-elements/solution.py
+++ my-folder/0347-top-k-frequent-elements/solution.py
@@ -1,24 +1,39 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # initialize count and freq and res
+        # count all the elements in the array 
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
+
+
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+
+
+        # then bucket sort
+        for num, cnt in count.items():
+            freq[cnt].append(num)
+
         res = []
-
-        # count of the elements
-        for n in nums:
-            count[n] = 1 + count.get(n, 0)
-
-        # group them by freq
-        for n, c in count.items():
-            freq[c].append(n)
-
-        # return the top k
-        for i in range(len(freq) - 1, 0, -1):
-            for n in freq[i]:
-                res.append(n)
+        # then return the top k in return order
+        for i in range(len(freq)-1, 0, -1):
+            for num in freq[i]:
+                res.append(num)
                 if len(res) == k:
                     return res
 
+        # nums = [1,1,1,2,2,3], k = 2
 
+        # count = {
+        #     1 : 3
+        #     2 : 2
+        #     1 : 1
+        # }
+
+        # bucket sort
+
+        # 0 | 1 | 2 | 3 | .. | len(k)
+        #     3   2   1
+
+        #     returns [1, 2]
 
