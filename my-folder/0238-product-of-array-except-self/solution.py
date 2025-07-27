8292fea607c9fa3da8f82f6cbb7d8e4cf38769e2
Kaleb Tsegaye <kfesaha15@gmail.com>
2025-07-27 10:23:37 +0000

    [LeetCode Sync] Runtime - 22 ms (66.29%), Memory - 23.1 MB (98.12%)

--- my-folder/0238-product-of-array-except-self/solution.py
+++ my-folder/0238-product-of-array-except-self/solution.py
@@ -1,13 +1,23 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+
+        # # Input: nums = [1,2,3,4]
         length = len(nums)
-        products = [1] * length
+        res = [1] * length
+
         for i in range(1, length):
-            products[i] = products[i-1] * nums[i-1]
+            res[i] = res[i - 1] * nums[i - 1]
 
         right = nums[-1]
+
         for i in range(length - 2, -1, -1):
-            products[i] *= right
+            res[i] *= right
             right *= nums[i]
+
         
-        return products
+        return res
+
+
+
+        # # [24,12,8,6]
